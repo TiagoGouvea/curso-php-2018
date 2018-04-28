8cc3933b3bb6bff047171a40f6ba25947d05dfd0
Hmmer <gal.almeida42@gmail.com>
2018-04-28 18:03:31 -0300

    mudancas

--- aula-1-guilherme/form.php
+++ aula-1-guilherme/form.php
@@ -1,9 +1,17 @@
 <?php
+    if(count($_POST)>0){
+        echo "<br>Olá ".$_POST['nome'].", voce tem ".$_POST['idade']." anos";
+    } else {
+
+        //var_dump($_POST);
+        ?>
+
+        <form method="post">
+            Nome: <input type="text" name="nome"><br>
+            Idade: <input type="text" name="idade"><br>
+            <input type="submit" name="Enviar">
+
+        </form>
+        <?php
+    }
     ?>
-
-<form>
-    Nome: <input type="text" name="nome"><br>
-    Idade: <input type="text" name="idade"><br>
-    <input type="submit" name="Enviar">
-
-</form>
