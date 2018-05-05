1e866490d7381d13f8070e3a550cd6da7960cef4
Hmmer <gal.almeida42@gmail.com>
2018-05-05 14:15:38 -0300

    modificações

--- aula-1-guilherme/form.php
+++ aula-1-guilherme/form.php
@@ -10,7 +10,6 @@
             Nome: <input type="text" name="nome"><br>
             Idade: <input type="text" name="idade"><br>
             <input type="submit" name="Enviar">
-
         </form>
         <?php
     }
--- aula-1-guilherme/segundo.php
+++ aula-1-guilherme/segundo.php
@@ -41,51 +41,6 @@ header('Content-Type: text/plain; charset=utf-8');
 //}
 
 
-//Array
-//(
-//    [0] => Array
-//    (
-//        [id] => 13
-//        [category_id] => 7
-//        [name] => Leaving Of Liverpool
-//        [description] => Leaving Of Liverpool
-//        [price] => 1.00
-//        [virtual] => 1
-//        [active] => 1
-//        [sort_order] => 13
-//        [created] => 2007-06-24 14:08:03
-//        [modified] => 2007-06-24 14:08:03
-//        [image] => NONE
-//    )
-//[1] => Array
-//    (
-//        [id] => 16
-//        [category_id] => 7
-//        [name] => Yellow Submarine
-//        [description] => Yellow Submarine
-//        [price] => 1.00
-//        [virtual] => 1
-//        [active] => 1
-//        [sort_order] => 16
-//        [created] => 2007-06-24 14:10:02
-//        [modified] => 2007-06-24 14:10:02
-//        [image] => NONE
-//    )
-//)
-//?>
-<!---->
-<!--<table>-->
-<?php
-//foreach ($products as $key => $value) {
-//    foreach ($value as $k => $v) {
-//        echo "<tr>";
-//        echo "<td>$k</td>"; // Get index.
-//        echo "<td>$v</td>"; // Get value.
-//        echo "</tr>";
-//    }
-//}
-//?>
-<!--</table>-->
 
 
 
