d75bb67a7bee513de36160e3002c373077395672
Hmmer <gal.almeida42@gmail.com>
2018-05-07 16:43:22 -0300

    praticas

--- aula-02-guilherme/api_sw.php
+++ aula-02-guilherme/api_sw.php
@@ -1,36 +1,35 @@
 <?php
 
-function CallAPI($method, $url, $data = false)
-{
-    $curl = curl_init();
-    switch ($method)
+    function CallAPI($method, $url, $data = false)
     {
-        case "POST":
-            curl_setopt($curl, CURLOPT_POST, 1);
+        $curl = curl_init();
+        switch ($method)
+        {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
 
-            if ($data)
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            break;
-        case "PUT":
-            curl_setopt($curl, CURLOPT_PUT, 1);
-            break;
-        default:
-            if ($data)
-                $url = sprintf("%s?%s", $url, http_build_query($data));
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+        // Optional Authentication:
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        //curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
     }
-    // Optional Authentication:
-    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    $result = curl_exec($curl);
-    curl_close($curl);
-    return $result;
-}
 
-$data = CallAPI("get","https://swapi.co/api/films/");
-
-//$api_sw = json_decode($data);
-//var_dump($api_sw);
-
-print_r($data);
+$data = CallAPI("get","https://swapi.co/api/people/");
+//
+$data = json_decode($data);
+//
+echo $data->results[0]->name;
