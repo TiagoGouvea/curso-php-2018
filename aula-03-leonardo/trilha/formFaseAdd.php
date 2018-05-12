58cb0b8a181cbd387c48ea8ffa28323e75b2fea8
leosalema <leosalema@yahoo.com.br>
2018-05-12 18:03:00 -0300

    inserido FormAdd e o index com list

--- aula-03-leonardo/trilha/formFaseAdd.php
+++ aula-03-leonardo/trilha/formFaseAdd.php
@@ -30,15 +30,15 @@
                 <form method="post">
                     <div class="form-group">
                         <label for="title">Titulo</label>
-                        <input type="text" class="form-control" id="title" placeholder="Insira o titulo" name="title">
+                        <input type="text" class="form-control" id="title" placeholder="Insira o titulo" name="title" required>
                     </div>
                     <div class="form-group">
                         <label for="description">Descrição</label>
-                        <textarea type="text" class="form-control" id="description" placeholder="Insira a descrição da fase" rows="10" name="description"></textarea>
+                        <textarea type="text" class="form-control" id="description" placeholder="Insira a descrição da fase" rows="10" name="description" required></textarea>
                     </div>
                     <div class="form-group">
                         <label for="numFase">Example select</label>
-                        <select class="form-control" id="numFase" name="numFase">
+                        <select class="form-control" id="numFase" name="numFase" required>
                             <option>1</option>
                             <option>2</option>
                             <option>3</option>
@@ -47,7 +47,7 @@
                         </select>
                     </div>
                     <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="status" name="status" value="1" checked>
+                        <input type="checkbox" class="form-check-input" id="status" name="status" value="1" checked required>
                         <label class="form-check-label" for="status">Ativo</label>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
@@ -66,5 +66,33 @@
  * Time: 15:02
  */
 
+    require 'conexao.php';
+
+
     var_dump($_POST);
+    $sql = "
+      INSERT INTO fase (
+        titulo, 
+        conteudo, 
+        ordem, 
+        ativa
+      ) 
+      VALUES (
+        :title, 
+        :description, 
+        :numFase, 
+        :status
+      )
+    ";
+
+    $std = $db->prepare($sql);
+    $std->bindParam(":title",$_POST['title'], PDO::PARAM_STR);
+    $std->bindParam(":description", $_POST['description'], PDO::PARAM_STR);
+    $std->bindParam(":numFase", $_POST['numFase'], PDO::PARAM_INT);
+    $std->bindParam(":status", $_POST['status'], PDO::PARAM_BOOL);
+    $success = $std->execute();
+    if ($success)
+        echo "Registro incluído";
+    else
+        echo "Falha ao incluir o registro";
 ?>
--- aula-03-leonardo/trilha/index.php
+++ aula-03-leonardo/trilha/index.php
@@ -1,3 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leonardo
+ * Date: 12/05/18
+ * Time: 15:01
+ */
+
+ require 'conexao.php';
+
+ try {
+     $sql = 'SELECT * FROM fase';
+     $std = $db->prepare($sql);
+     $success = $std->execute();
+
+     if ($success) {
+         echo "Sucesso";
+         $result = $std->fetchAll(PDO::FETCH_OBJ);
+     } else {
+         echo "Erro na busca";
+     }
+
+
+     echo "<pre>";
+     var_dump($result);
+     echo "</pre>";
+
+ } catch (Exception $e) {
+    var_dump($e);
+ }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -26,14 +57,30 @@
                     </div>
                 </nav>
             </div>
+            <div class="content">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Ordem</th>
+                        <th scope="col">Titulo</th>
+                        <th scope="col">Conteúdo</th>
+                        <th scope="col">Ativo</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                        echo "
+                            <tr>
+                                <th scope='row'>1</th>
+                                <td>{$result[0]->titulo}</td>
+                            </tr>
+                        ";
+                    ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </body>
 </html>
 
-<?php
-/**
- * Created by PhpStorm.
- * User: leonardo
- * Date: 12/05/18
- * Time: 15:01
- */
