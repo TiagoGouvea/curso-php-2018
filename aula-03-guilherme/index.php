160fe4db17e48b768f320e6a385504cbe8ffc151
Hmmer <gal.almeida42@gmail.com>
2018-05-14 23:06:57 -0300

    Melhorias no edit e Index

--- aula-03-guilherme/index.php
+++ aula-03-guilherme/index.php
@@ -29,7 +29,7 @@ require 'conexao.php';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Pagina Principal</title>
 </head>
 <body class="text-center">
 <div class="container">
