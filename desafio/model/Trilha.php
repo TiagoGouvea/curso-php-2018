7008f12db81f9cb0a2373b5c82e42a09c64544e7
geronimoapb <38816572+geronimoapb@users.noreply.github.com>
2018-05-19 17:39:00 -0300

    Atualizando

--- desafio/model/Trilha.php
+++ desafio/model/Trilha.php
@@ -1,7 +1,43 @@
 <?php
-namespace Model;
 
+global $db;
 class Trilha{
-    // $db global
-    //global $db;
+
+    public $titulo;
+    public $descricao;
+    public $ordem;
+    public $ativa;
+
+    static function cadastrar($post){
+        global $db;
+        try {
+            $sql = "
+                INSERT INTO trilha (
+                    titulo, 
+                    descricao, 
+                    ordem, 
+                    ativa
+                ) 
+                VALUES (
+                    :title, 
+                    :description, 
+                    :numTrilha, 
+                    :status
+                )
+                ";
+
+            $std = $db->prepare($sql);
+            $std->bindParam(":title", $_POST['title'], PDO::PARAM_STR);
+            $std->bindParam(":description", $_POST['description'], PDO::PARAM_STR);
+            $std->bindParam(":numTrilha", $_POST['numTrilha'], PDO::PARAM_INT);
+            $std->bindParam(":status", $_POST['status'], PDO::PARAM_BOOL);
+            $success = $std->execute();
+            if ($success)
+                echo "<p>Registro incluído</p>";
+            else
+                echo "<p>Falha ao incluir o registro</p>";
+        } catch (Exception $e) {
+            echo "<p>Erro ao de comunicação com o servidor</p>";
+        }
+    }
 }
