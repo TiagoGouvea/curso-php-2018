802e4af0933f2a695d4cfe17efa395c769e9fa2b
BernardoCosta204 <33205115+BernardoCosta204@users.noreply.github.com>
2018-05-19 16:49:41 -0300

    Aula 04

---
+++ aula-04-bernardo/view/form.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fomulário</title>
+
+</head>
+<body>
+<form method="post">
+    Nome:
+    <input type="text" name="nome">
+    <input type="submit">
+</form>
+</body>
